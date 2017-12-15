867de6b6976e6543eb37f1e02d4ed7210829a343
Karll <hankikab@gmail.com>
2017-12-15 17:24:35 +0900

    Customer 모델 디비반영됨

--- djangoshop/models.py
+++ djangoshop/models.py
@@ -4,11 +4,11 @@ class Customer(models.Model):
     # 고객아이디
     CustID      = models.CharField(max_length=15)
     # 고객이름
-    CustName    = models.TextField()
+    CustName    = models.TextField(null=False)
     # 고객패스워드
-    CustPwd     = models.TextField()
+    CustPwd     = models.TextField(null=False)
     # 고객 이메일
-    CustEmail   = models.TextField()
+#    CustEmail   = models.TextField(null=False)
     # 고객 타입 A:관리자 B:판매자 C:소비자
     CustType    = models.CharField(max_length=1)
     
