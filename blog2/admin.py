9233833caab9e252794b0b19f13035dc49c51566
Kor-Karll <hankikab@naver.com>
2017-12-20 16:38:26 +0900

    blog2 앱,모델 등록

--- blog2/admin.py
+++ blog2/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog2.models import Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'modify_date')
+    list_filter = ('modify_date')
+    search_fields = ('title', 'conten')
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Post, PostAdmin)
